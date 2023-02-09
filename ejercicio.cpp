b28a36d7926daec59e9229443028954f142f316c
Wilson Liñeiro <36859716+wilson1998@users.noreply.github.com>
2023-02-08 20:14:04 -0500

    EJEMPLO1

---
+++ ejercicio.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <windows.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+using namespace std;
+
+main(){
+      
+     for(int i=0; i<=10; i++){ // RECORRER MULTIPLICANDO
+          
+     	
+       for( int j=0;j<=10;j++){ //RECORRER EL MULTIPLICADOR
+       
+                 
+       	    	cout<< j<<" X "<<i<<" = "<< j*i<<"\t"; 
+       	    	 	    		   	
+	     
+	   }
+	  
+	   cout<<"\n";
+        
+     	
+	 }
+	 
+	 
+   
+
+	
+	
+}
+
+
